fdde5b971283378c69e7f13de40d3c475720ebe8
Max Bonas <113984950+MaxBonas@users.noreply.github.com>
2025-06-14 22:03:09 +0200

    Add Power, Factorial and Gcd functions with tests

--- AppCs.Library/Calculator.cs
+++ AppCs.Library/Calculator.cs
@@ -14,5 +14,38 @@ namespace AppCs.Library
                 throw new DivideByZeroException();
             return a / b;
         }
+
+        public int Power(int baseValue, int exponent)
+        {
+            if (exponent < 0)
+                throw new ArgumentException("Exponent must be non-negative", nameof(exponent));
+            int result = 1;
+            for (int i = 0; i < exponent; i++)
+                result *= baseValue;
+            return result;
+        }
+
+        public int Factorial(int n)
+        {
+            if (n < 0)
+                throw new ArgumentException("n must be non-negative", nameof(n));
+            int result = 1;
+            for (int i = 2; i <= n; i++)
+                result *= i;
+            return result;
+        }
+
+        public int Gcd(int a, int b)
+        {
+            a = Math.Abs(a);
+            b = Math.Abs(b);
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
     }
 }
